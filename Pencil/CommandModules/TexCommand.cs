ec370c7a0a3af95b5dfb28eccc8c66bb6772188a
Oliver Booth <oliver@booth.dev>
2025-02-19 16:45:31 +0000

    feat: add log output for /tex

--- Pencil/CommandModules/TexCommand.cs
+++ Pencil/CommandModules/TexCommand.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.RegularExpressions;
 using Discord;
 using Discord.Interactions;
+using Microsoft.Extensions.Logging;
 using Pencil.Configuration;
 using Pencil.Services;
 using Color = Discord.Color;
@@ -12,16 +13,19 @@ namespace Pencil.CommandModules;
 /// </summary>
 internal sealed class TexCommand : InteractionModuleBase<SocketInteractionContext>
 {
+    private readonly ILogger<TexCommand> _logger;
     private readonly ConfigurationService _configurationService;
     private readonly LatexService _latexService;
 
     /// <summary>
     ///     Initializes a new instance of the <see cref="TexCommand" /> class.
     /// </summary>
+    /// <param name="logger">The logger.</param>
     /// <param name="configurationService">The configuration service.</param>
     /// <param name="latexService">The LaTeX rendering service.</param>
-    public TexCommand(ConfigurationService configurationService, LatexService latexService)
+    public TexCommand(ILogger<TexCommand> logger, ConfigurationService configurationService, LatexService latexService)
     {
+        _logger = logger;
         _configurationService = configurationService;
         _latexService = latexService;
     }
@@ -35,6 +39,7 @@ internal sealed class TexCommand : InteractionModuleBase<SocketInteractionContex
             return;
         }
 
+        _logger.LogInformation("Rendering TeX for message {Id} ({Message})", message.Id, message.Content);
         using LatexService.RenderResult result = _latexService.Render(message.Content);
         if (!result.Success)
         {
@@ -46,14 +51,16 @@ internal sealed class TexCommand : InteractionModuleBase<SocketInteractionContex
             await RespondAsync(embed: embed.Build(), ephemeral: true).ConfigureAwait(false);
             return;
         }
-        
+
         await RespondWithFileAsync(result.ImageStream, "output.png", ephemeral: true).ConfigureAwait(false);
     }
 
     [SlashCommand("tex", "Renders a TeX expression.")]
     public async Task TexCommandAsync(
-        [Summary("expression", "The expression to render")] string expression,
-        [Summary("spoiler", "Whether to render this image as a spoiler. Defaults to false.")] bool spoiler = false)
+        [Summary("expression", "The expression to render")]
+        string expression,
+        [Summary("spoiler", "Whether to render this image as a spoiler. Defaults to false.")]
+        bool spoiler = false)
     {
         if (Context.Guild is { } guild &&
             _configurationService.TryGetGuildConfiguration(guild, out GuildConfiguration? guildConfiguration))
@@ -68,6 +75,7 @@ internal sealed class TexCommand : InteractionModuleBase<SocketInteractionContex
             }
         }
 
+        _logger.LogInformation("Rendering TeX for expression {Expression}", expression);
         using LatexService.RenderResult result = _latexService.Render(expression);
 
         if (!result.Success)
